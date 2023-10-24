fa36179bbd23e65e1445f6a171b31218c6ce972f
TonyNavas <tonynavas777@gmail.com>
2023-10-23 18:20:12 -0600

    Definiendo las rutas api

--- routes/api.php
+++ routes/api.php
@@ -1,31 +1,36 @@
 <?php
 
-use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\UserController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\API\CamionController;
+use App\Http\Controllers\API\MineroController;
+use App\Http\Controllers\API\ConductorController;
+use App\Http\Controllers\API\CooperativaController;
+use App\Http\Controllers\API\ProcedenciaController;
+use App\Http\Controllers\API\VerificacionController;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
-Route::post('login',[UserController::class,'loginUser']);
+Route::post('login', [UserController::class, 'loginUser']);
 
-Route::post('/auth/register',[AuthController::class,'register']);
-Route::post('/auth/login',[AuthController::class,'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
 
-Route::group(['middleware' => 'auth:sanctum'],function(){
-    Route::get('/user',[UserController::class,'index']);
-    Route::get('/auth/logout',[AuthController::class,'logout']);
+
+// Rutas protejidas por token
+Route::group(['middleware' => 'auth:sanctum'], function () {
+
+    Route::get('/auth/logout', [AuthController::class, 'logout']);
+
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/cooperativas', [CooperativaController::class, 'index']);
+    Route::get('/mineros', [MineroController::class, 'index']);
+    Route::get('/procedencias', [ProcedenciaController::class, 'index']);
+    Route::get('/conductores', [ConductorController::class, 'index']);
+    Route::get('/camiones', [CamionController::class, 'index']);
+    Route::get('/verificacion', [VerificacionController::class, 'index']);
 });
